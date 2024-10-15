9d1ca065d4933908092b7560ae10e418afa83397
choijeonghye <rojenna@choijeonghyes-MacBook-Pro.local>
2024-10-15 23:11:16 +0900

    feat: websocket 연결

--- scripts/loadUnityScreen.js
+++ scripts/loadUnityScreen.js
@@ -4,20 +4,14 @@
 function loadUnityScreen() {
   const lidarHostInput = document.getElementById("lidarServerInput");
   const unityIframe = document.getElementById("lidarDisplay");
-  // const placeholderImage = document.getElementById("placeholderImage");
 
   const host = lidarHostInput.value.trim();
   if (host) {
     unityIframe.src = host;
     unityIframe.style.display = "block";
-    // placeholderImage.style.display = "none";
   } else {
     alert("Please enter a valid host address.");
   }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  const connectLidarServerButton =
-    document.getElementById("connectLidarServer");
-  connectLidarServerButton.addEventListener("click", loadUnityScreen);
-});
+export default loadUnityScreen;
